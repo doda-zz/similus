fcb4743ee0849cf446d479f5c5995fa418fd5616
Dominik Dario <dominik@doda.co>
2012-04-19 04:02:54 +0200

    Image creation works

--- similus/animals.py
+++ similus/animals.py
@@ -1,5 +1,19 @@
 from similus import compare, get_data, compare_data
-import os
+import ImageFont
+import ImageDraw
+import ImageChops
+import Image
+from path import path
+
+def autocrop(im, bgcolor):
+    if im.mode != "RGB":
+        im = im.convert("RGB")
+    bg = Image.new("RGB", im.size, bgcolor)
+    diff = ImageChops.difference(im, bg)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+    return None # no contents
 
 class Crab(object):
     def __init__(self):
@@ -21,7 +35,7 @@ class Crab(object):
             raise IndexError('No images loaded')
         image_data = get_data(image)
         comparisons = ((other_image, compare_data(image_data, other_image_data)) for
-                       other_image_data, other_imagein self.images)
+                       other_image_data, other_image in self.images)
         if sort:
             return sorted(comparisons, key=lambda x:x[1])
         return comparisons
@@ -32,12 +46,32 @@ class Crab(object):
 class Otter(object):
     def __init__(self, directory=None):
         if directory:
-            self.load_images(os.listdir(directory))
+            self.load_images(path(directory).files())
 
-    def create_images(font, alphabet, directory):
-        pass
+    def create_images(self, font, alphabet, directory, font_size=14):
+        font = ImageFont.truetype(font, font_size)
+        directory = path(directory)
+        directory.makedirs_p()
+        for char in alphabet:
+            self.create_char(font, char, directory, font_size)
+
+    def create_char(self, font, char, directory, font_size):
+        im_size = (font_size*2, font_size*2)
+        img = Image.new("RGB", im_size, (255,255,255))
+        draw = ImageDraw.Draw(img)
+        draw.text((2, 2), char, (0,0,0), font=font)
+        img = autocrop(img, 'white')
+#        img.show()
+        img_path = path(directory).joinpath('%05d.png' % ord(char))
+        img.save(img_path)
+        print img_path
 
     def load_images(self, image_files):
         for f in image_files:
             pass
             
+if __name__ == '__main__':
+    import string
+    alphabet = string.letters + string.digits
+    otter = Otter()
+    otter.create_images('/home/ddd/Dropbox/prog/sc2wow/bl.ttf', alphabet=alphabet, directory='similus_images')
